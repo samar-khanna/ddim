6025b47f6086fa6bab33a85ec659b7d1f840df89
Samar Khanna <samar.khanna@gmail.com>
2022-11-17 22:18:04 -0800

    Add snr image space loss

--- functions/losses.py
+++ functions/losses.py
@@ -15,12 +15,29 @@ def noise_estimation_loss(model,
         return (e - output).square().sum(dim=(1, 2, 3)).mean(dim=0)
 
 
+def snr_image_loss(
+        model,
+        x0: torch.Tensor,
+        t: torch.LongTensor,
+        e: torch.Tensor,
+        b: torch.Tensor, keepdim=False
+):
+    a = (1 - b).cumprod(dim=0).index_select(0, t).view(-1, 1, 1, 1)
+    x = x0 * a.sqrt() + e * (1.0 - a).sqrt()
+    output = model(x, t.float())
+
+    if keepdim:
+        return (a/(1.0-a)).clamp(min=1.0) * (x0 - output).pow(2).sum(dim=(1, 2, 3))
+    else:
+        return (a/(1.0-a)).clamp(min=1.0) * (x0 - output).pow(2).sum(dim=(1, 2, 3)).mean(dim=0)
+
+
 loss_registry = {
     'simple': noise_estimation_loss,
     'unet': noise_estimation_loss,
     'vit': noise_estimation_loss,
     'vit_mae': noise_estimation_loss,
     'vit_mae_temb': noise_estimation_loss,
-    'umae': noise_estimation_loss,
+    'umae': snr_image_loss,
     'uvit': noise_estimation_loss,
 }
