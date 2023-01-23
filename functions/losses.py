dc4bbc7c68881b98b52be5092efaab99db5159da
Samar Khanna <samar.khanna@gmail.com>
2023-01-23 15:29:38 -0800

    Fix trunc image loss

--- functions/losses.py
+++ functions/losses.py
@@ -28,16 +28,18 @@ def snr_image_loss(
     # a = (1 - b).cumprod(dim=0).index_select(0, t).view(-1, 1, 1, 1)
     # x = x0 * a.sqrt() + e * (1.0 - a).sqrt()
 
-    a = noise_schedule.marginal_alpha(t).view(-1, 1, 1, 1)
-    sigma = noise_schedule.marginal_std(t).view(-1, 1, 1, 1)
+    # Ref: https://github.com/google-research/google-research/blob/32363faa531d8386a5733b54803893f957472e1e/diffusion_distillation/diffusion_distillation/dpm.py#L222
     logsnr = 2.0 * noise_schedule.marginal_lambda(t).view(-1, 1, 1, 1)
+    a = logsnr.sigmoid().sqrt()
+    sigma = (-logsnr).sigmoid().sqrt()
     x = x0 * a + e * sigma
 
     x0_pred = model(x, t.float())
+    # eps = (x_t - alpha*x0)/sigma.
     eps_pred = (1. + logsnr.exp()).sqrt() * (x - x0 * (1. + (-logsnr).exp()).rsqrt())
 
-    x_mse = (x0 - x0_pred).pow(2).mean(dim=(1, 2, 3))  # (N,)
-    eps_mse = (e - eps_pred).pow(2).mean(dim=(1, 2, 3))  # (N,)
+    x_mse = (x0_pred - x0).pow(2).mean(dim=(1, 2, 3))  # (N,)
+    eps_mse = (eps_pred - e).pow(2).mean(dim=(1, 2, 3))  # (N,)
     batch_loss = torch.maximum(x_mse, eps_mse)
 
     # a = a.view(-1)
