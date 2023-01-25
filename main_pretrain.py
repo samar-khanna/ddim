5aa94a0bf58c515f5c8726ded33331627a9441f1
Samar Khanna <samar.khanna@gmail.com>
2023-01-25 12:25:43 -0800

    Stupid arg namespace clash

--- main_pretrain.py
+++ main_pretrain.py
@@ -50,8 +50,8 @@ def get_args_parser():
                         help='Accumulate gradient iterations (for increasing the effective batch size under memory constraints)')
 
     # Model parameters
-    # parser.add_argument('--model_type', default=None, choices=['group_c', 'temporal', 'vanilla'],
-    #                     help='Use channel model')
+    parser.add_argument('--model_type', default=None, choices=['group_c', 'temporal', 'vanilla'],
+                        help='Use channel model')
     # parser.add_argument('--model', default='mae_vit_large_patch16', type=str, metavar='MODEL',
     #                     help='Name of model to train')
 
